50574a526401c19a441e493bc36b2abd55149d2c
pberkman <patriciaberkman@Patricias-MacBook-Pro.local>
2014-08-25 10:52:21 -0400

    added nag highlight feature
    font stays white til another section is clicked

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 //notes on how to switch between visible and hidden for profile info
 $(document).ready(function() {
-
+	
+	// About section - click each header to make their info appear and disappear
 	$('#learn, #skills, #interests, #profile').click(function() {
 		if ($(this).siblings().css('display') == 'none') {
+			// stop displaying any info already showing
 			$(".aboutInfo").css('display','none');
 			$(this).siblings().css('display','inline-block');
 		}
-
 		else {
 			$(this).siblings().css('display','none');
 		}
 	});
 
+	// Project Section - show paragraphs when hovering over pic
 	$(".proj").mouseenter(function() {
 		$(this).children(".projHover").show();
 	})
@@ -21,23 +23,29 @@ $(document).ready(function() {
 
 	// Scroll to each section
 	$("#homeNav").click(function() {
-		$('#home').ScrollTo();
+		$(".navItems").css('color','#000000');
+		$(this).css('color','white');
+		$("#home").ScrollTo();
 	});
 	$("#aboutNav").click(function() {
-		$('#about').ScrollTo();
+		$(".navItems").css('color','#000000');
+		$(this).css('color','white');
+		$("#about").ScrollTo();
 	});
 	$("#projectsNav").click(function() {
-		$('#projects').ScrollTo();
+		$(".navItems").css('color','#000000');
+		$(this).css('color','white');
+		$("#projects").ScrollTo();
 	});
 	$("#contactNav").click(function() {
-		$('#contact').ScrollTo();
+		$(".navItems").css('color','#000000');
+		$(this).css('color','white');
+		$("#contact").ScrollTo();
 	});
 
-	$('.navItems').mouseenter(function() {
-		$(this).css('color', 'white');
-	})
-	.mouseleave(function() {
-		$(this).css('color', '#000000');
+	// Navigation bar - Change font color to white when hovering
+	$('.navItems').hover(function() {
+		$(this).toggleClass('linkStyle');
 	});
 
 }); //end ready
