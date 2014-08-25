14324839d78578827e723f0fc79cf9c8a0543f26
pberkman <patriciaberkman@Patricias-MacBook-Pro.local>
2014-08-25 14:58:02 -0400

    scroll addition

--- app.js
+++ app.js
@@ -22,30 +22,38 @@ $(document).ready(function() {
 	});
 
 	// Scroll to each section
-	$("#homeNav").click(function() {
-		$(".navItems").css('color','#000000');
+	$("nav a").click(function(evn) {
+		evn.preventDefault();
+		$("nav a").css('color','#000000');
+		$("html, body").scrollTo(this.hash, this.hash);
 		$(this).css('color','white');
-		$("#home").ScrollTo();
-	});
-	$("#aboutNav").click(function() {
-		$(".navItems").css('color','#000000');
-		$(this).css('color','white');
-		$("#about").ScrollTo();
-	});
-	$("#projectsNav").click(function() {
-		$(".navItems").css('color','#000000');
-		$(this).css('color','white');
-		$("#projects").ScrollTo();
-	});
-	$("#contactNav").click(function() {
-		$(".navItems").css('color','#000000');
-		$(this).css('color','white');
-		$("#contact").ScrollTo();
 	});
 
-	// Navigation bar - Change font color to white when hovering
-	$('.navItems').hover(function() {
-		$(this).toggleClass('linkStyle');
-	});
+	// color the navigation font while scrolling through each section
+	var aChildren = $("nav li").children(); // find the a children of the list items
+    var aArray = []; // create the empty aArray
+    for (var i=0; i < aChildren.length; i++) {
+        var aChild = aChildren[i];
+        var ahref = $(aChild).attr('href');
+        aArray.push(ahref);
+    }
+
+	$(window).scroll(function() {
+
+        var windowPos = $(window).scrollTop(); // get the position of where the window is from the top
+        var windowHeight = $(window).height(); // get the height of the window
+        var docHeight = $(document).height();
+
+        for (var i=0; i < aArray.length; i++) {
+            var theID = aArray[i];
+            var sectionPos = $(theID).offset().top; // get the offset of the section from the top of page
+            var sectionHeight = $(theID).height(); // get the height of the section in question
+            if (windowPos >= sectionPos && windowPos < (sectionPos + sectionHeight)) {
+                $("a[href='" + theID + "']").css('color','white');
+            } else {
+                $("a[href='" + theID + "']").css('color','#000000');
+            }
+        }
+	}); // end window scroll
 
 }); //end ready
