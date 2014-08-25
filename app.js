26cd4828c17e533f980e668f0479555698e88c1e
pberkman <patriciaberkman@Patricias-MacBook-Pro.local>
2014-08-24 20:01:28 -0400

    scroll feature

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 //notes on how to switch between visible and hidden for profile info
 $(document).ready(function() {
 
-
-
 	$('#learn, #skills, #interests, #profile').click(function() {
 		if ($(this).siblings().css('display') == 'none') {
 			$(".aboutInfo").css('display','none');
@@ -14,9 +12,6 @@ $(document).ready(function() {
 		}
 	});
 
-
-
-
 	$(".proj").mouseenter(function() {
 		$(this).children(".projHover").show();
 	})
@@ -24,7 +19,18 @@ $(document).ready(function() {
 		$(this).children(".projHover").hide();
 	});
 
-	window.location.hash = "#about";
-
+	// Scroll to each section
+	$("#homeNav").click(function() {
+		$('#home').ScrollTo();
+	});
+	$("#aboutNav").click(function() {
+		$('#about').ScrollTo();
+	});
+	$("#projectsNav").click(function() {
+		$('#projects').ScrollTo();
+	});
+	$("#contactNav").click(function() {
+		$('#contact').ScrollTo();
+	});
 
 }); //end ready
